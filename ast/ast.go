aaf7065d7a76e4a033955f55aab2ecfe1dd0af75
lzcmian <lzcweb88@gmail.com>
2024-12-21 13:23:25 +0800

    2.8.5: 成功解析调用表达式

--- ast/ast.go
+++ ast/ast.go
@@ -236,3 +236,27 @@ func (fl *FunctionLiteral) String() string {
 	out.WriteString(fl.Body.String())
 	return out.String()
 }
+
+type CallExpression struct {
+	Token     token.Token
+	Function  Expression // 标识符或函数字面量
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode()      {}
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	var out bytes.Buffer
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+
+}

b4f492b25d77202035c0dd5694e9b619bd9b9820
lzcmian <lzcweb88@gamil.com>
2024-12-25 20:51:47 +0800

    3.4.2　整数

--- object/object.go
+++ object/object.go
@@ -1,8 +1,21 @@
 package object
 
+import "fmt"
+
 type ObjectType string
+
+const (
+	INTERGER_OBJ = "INTEGER"
+)
 
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTERGER_OBJ }

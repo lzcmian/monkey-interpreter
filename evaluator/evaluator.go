9c9826ca6794b7f60963bf2ae285875bba256a6a
lzcmian <lzcweb88@gmail.com>
2025-01-01 10:51:09 +0800

    3.5.4　空值

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"monkey-interpreter/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -13,6 +19,12 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		if node.Value == true {
+			return TRUE
+		} else {
+			return FALSE
+		}
 	}
 	return nil
 }

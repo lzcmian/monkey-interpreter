8f9ca04e69fea217afe1dc10ba0bceb8cdd2a997
lzcmian <lzcweb88@gamil.com>
2025-02-20 22:44:25 +0800

    4.3 内置函数len

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -89,14 +89,17 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 // applyFunction 求值函数调用, 首先创建一个新的内部环境, 其 outer 指针指向函数定义的环境.
 // 然后将参数值绑定到内部环境中, 最后调用函数获取返回值.
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
+	switch fn := fn.(type) {
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+	case *object.Builtin:
+		return fn.Fn(args...)
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
 
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-	return unwrapReturnValue(evaluated)
 }
 
 func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
@@ -135,11 +138,17 @@ func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Ob
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
 	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: " + node.Value)
+	if ok {
+		return val
 	}
 
-	return val
+	builtin, ok := builtins[node.Value]
+	if ok {
+		return builtin
+	}
+
+	return newError("identifier not found: " + node.Value)
+
 }
 
 func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Object {
